d362c216275e71c820b595166ade82e2563c26e0
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-10-19 23:41:48 +0900

    feat(calculator): 기능요구사항 1번 기본 구분자(, :)을 이용한 문자열 덧셈 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력 주세요.\n");
+        String input = Console.readLine();
+        String[] numbers = input.split("[,:]");
+        int result = 0;
+        for (String number : numbers) {
+            result += Integer.parseInt(number);
+        }
+        System.out.println("결과 : " + result);
     }
 }
