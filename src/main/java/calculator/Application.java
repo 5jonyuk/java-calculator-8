4b234facbe75c57c1538a65db01797ef701bd394
ohjonghyuk <ohjonghyuk0717@naver.com>
2025-10-20 00:27:01 +0900

    feat(calculator): 기능 요구사항 2번 커스텀 문자열 계산 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,15 +2,29 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("덧셈할 문자열을 입력 주세요.\n");
         String input = Console.readLine();
-        String[] numbers = input.split("[,:]");
+        input = input.replace("\\n","\n"); // 콘솔에서는 실제로 "\n"입력을 "\\n"로 인식하기에 replace 사용
+        String regex;
+
+        if (input.startsWith("//")) { // 커스텀 모드
+            int delimiterEndIndex = input.indexOf("\n");
+            String customDelimiter = input.substring(2, delimiterEndIndex);
+            regex = "[,:]|" + Pattern.quote(customDelimiter);
+            input = input.substring(delimiterEndIndex + 1);
+        }
+        else { // 일반 모드
+            regex = "[,:]";
+        }
+        String[] numbers = input.split(regex);
         int result = 0;
         for (String number : numbers) {
-            result += Integer.parseInt(number);
+             result += Integer.parseInt(number);
         }
         System.out.println("결과 : " + result);
     }
